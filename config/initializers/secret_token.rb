fd284b330e107d5f2e6aa46b4af1090e3bbfc130
vimtaku <vimtaku@gmail.com>
2014-03-02 10:33:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TryDevise::Application.config.secret_key_base = '0743b99c414d402aec047b6fed44ae0354e11038b310a10b0e8073778e08cdfca2e62b5c9d7dfa65201a28fb235c7e3339fcd44e151d109a5a312d22fcab9202'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TryDevise::Application.config.session_store :cookie_store, key: '_try_devise_session'
